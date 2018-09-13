ad097b45283a872e31eb5cddb1254c3979742646
Ronaldo Moreira <ronaldo.moreira.rm@gmail.com>
2018-09-13 12:35:26 -0300

    Criando operacao de instanciacao - Categoria

--- src/main/java/com/moreiraronaldo/cursomc/CursomcApplication.java
+++ src/main/java/com/moreiraronaldo/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package com.moreiraronaldo.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.moreiraronaldo.cursomc.domain.Categoria;
+import com.moreiraronaldo.cursomc.reposotories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaReporisory;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaReporisory.save(Arrays.asList(cat1, cat2));
+		
 	}
 }
