2101b676c54c34628658c93582958b94f37f5755
Ronaldo Moreira <ronaldo.moreira.rm@gmail.com>
2018-09-25 00:31:18 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/moreiraronaldo/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.moreiraronaldo.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moreiraronaldo.cursomc.domain.Cliente;
+import com.moreiraronaldo.cursomc.reposotories.ClienteRepository;
+import com.moreiraronaldo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		
+		if(obj == null ) {
+			throw new ObjectNotFoundException("Objeto nao encontrado - ID: "  + id
+					+  ", Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;
+	}
+}
