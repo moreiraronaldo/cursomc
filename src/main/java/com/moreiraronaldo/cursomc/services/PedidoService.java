b53b96e295914d601ffb0bebe92557ca86558653
Ronaldo Moreira <ronaldo.moreira.rm@gmail.com>
2018-10-26 18:09:45 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/moreiraronaldo/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.moreiraronaldo.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moreiraronaldo.cursomc.domain.Pedido;
+import com.moreiraronaldo.cursomc.reposotories.PedidoRepository;
+import com.moreiraronaldo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		
+		if(obj == null ) {
+			throw new ObjectNotFoundException("Objeto nao encontrado - ID: "  + id
+					+  ", Tipo: " + Pedido.class.getName());
+		}
+		
+		return obj;
+	}
+}
